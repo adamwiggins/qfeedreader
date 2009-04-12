19f051761c440e5b960fbb933aca92f88c867858
Adam Wiggins <adam@heroku.com>
2009-04-12 13:44:49 -0700

    delayed_job plugin and table

---
+++ db/migrate/20090412203342_create_delayed_jobs.rb
@@ -0,0 +1,19 @@
+class CreateDelayedJobs < ActiveRecord::Migration
+  def self.up
+    create_table :delayed_jobs do |t|
+      t.integer  :priority, :default => 0
+      t.integer  :attempts, :default => 0
+      t.text     :handler
+      t.string   :last_error
+      t.datetime :run_at
+      t.datetime :locked_at
+      t.datetime :failed_at
+      t.string   :locked_by
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :delayed_jobs
+  end
+end
