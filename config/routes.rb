c7b718605422feabb9df17d72cb36da1f11d6a13
Ryan Tomayko <rtomayko@gmail.com>
2009-04-14 19:49:28 -0700

    feed refresh URL naming
    Use "/feeds/:id/refresh" instead of "/feeds/refresh/:id" and
    "/feeds/refresh" instead of "/feeds/refresh_all".

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect '/feeds/refresh_all', :controller => 'feeds', :action => 'refresh_all'
+  map.connect '/feeds/refresh', :controller => 'feeds', :action => 'refresh_all', :method => 'post'
   map.resources :feeds
 
   map.root :controller => 'feeds'
-
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  map.connect '/feeds/:id/refresh', :controller => 'feeds', :action => 'refresh'
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,10 @@
 function refresh_feed(link) {
-  new Ajax.Request('/feeds/refresh/' + link.getAttribute('feed_id'), { method: 'post' })
+  new Ajax.Request('/feeds/' + link.getAttribute('feed_id') + '/refresh', { method: 'post' })
   spin_and_wait(link)
 }
 
 function refresh_all() {
-  new Ajax.Request('/feeds/refresh_all', { method: 'post' })
+  new Ajax.Request('/feeds/refresh', { method: 'post' })
   $$('.feed .refresh a').each(function(link) {
     spin_and_wait(link)
   })
