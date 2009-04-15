b7c364589fb3b128121024a2abe9eb5b55c05c5e
Adam Wiggins <adam@heroku.com>
2009-04-15 00:59:08 -0700

    move refresh route above the resource map

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect '/feeds/refresh', :controller => 'feeds', :action => 'refresh_all', :method => 'post'
+  map.connect '/feeds/:id/refresh', :controller => 'feeds', :action => 'refresh'
   map.resources :feeds
 
   map.root :controller => 'feeds'
-  map.connect '/feeds/:id/refresh', :controller => 'feeds', :action => 'refresh'
 end
